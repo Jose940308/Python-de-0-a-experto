65a7a2e24fb5e6ddbfc105e0cad37b0cc69286ac
Jose Lopez Alvarez <joseloal94@live.com>
2025-08-21 17:26:08 -0600

    Add files via upload

---
+++ Sección5_Entrada_de_Datos_Python/5_2_Entrada_datos_consola.py
@@ -0,0 +1,7 @@
+#Entrada de datos por consola
+nombre = input('Introduce tu nonbre: ')
+print(f'Recibiendo el valor de nombre: {nombre}')
+
+#Pedir la edad al usuario (entra como cadena y lo convertiimos a numero)
+edad = int(input('Introduce tu edad: '))
+print(f'Tu edad es: {edad}, y en un año tendrás {edad + 1}')
