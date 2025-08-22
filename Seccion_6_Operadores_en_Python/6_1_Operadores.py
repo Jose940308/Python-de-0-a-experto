51fc1dcb9198b760e01c86b398a93bea0fb23663
Jose Lopez Alvarez <joseloal94@live.com>
2025-08-21 20:51:08 -0600

    Adding Operadores

---
+++ Seccion_6_Operadores_en_Python/6_1_Operadores.py
@@ -0,0 +1,31 @@
+# Operadores aritméticos
+a = 10
+b = 3
+
+# Suma +
+suma = a + b
+print(f'Suma: {suma}')
+
+# Resta -
+resta = a - b
+print(f'Resta: {resta}')
+
+# Multiplicación *
+multiplicacion = a * b
+print(f'Multiplicación: {multiplicacion}')
+
+# Division / (Retorna un tipo float)
+division = a / b
+print(f'Divisón: {division:.2f}') #Para darle un formato de 2 digitos y de tipo flotante, se usa :2f
+
+# Division entera (//)
+division_entera = a // b
+print(f'Division entera: {division_entera}')
+
+# Modulo (%) residuo de la division
+modulo = a % b
+print(f'Residuo de la division: {modulo}')
+
+# Exponente (potencia) **
+exponente = a ** b #10 elevado a la 3 = 10*10*10=1000
+print(f'Exponente: {exponente}')
