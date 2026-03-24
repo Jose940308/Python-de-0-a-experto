b1758777c8ed9bebb0b8fa44e939f7b577c2be11
Jose Lopez Alvarez <joseloal94@live.com>
2026-03-23 20:24:12 -0700

    Adding operadores_comparacion.py file

---
+++ Seccion_6_Operadores_en_Python/operadores_comparacion.py
@@ -0,0 +1,19 @@
+print('*** Operadores Comparaacion (Relacionales ***)')
+a, b= 7, 5
+print(f'Valor inicial a: {a}, {b}')
+
+# Operador igual ==
+resultado = a == b
+print(f'Resultado a == b es: {resultado}')
+
+# Operador diferente !=
+resultado= a != b
+print(f'Resultado a =! b es: {resultado}')
+
+# Operador menor que
+resultado = a > b
+print(f'Resultado a > b es: {resultado}')
+
+# Operador mayor que
+resultado = a < b
+print(f'Resultado a > b es: {resultado}')
